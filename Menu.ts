143b4a72b6ca53b2806db87cec64f2257bee4d7d
Gabrielle De Castro <gabrielleclvs@users.noreply.github.com>
2025-11-12 22:13:16 -0300

    Implementação inicial do controller

--- Menu.ts
+++ Menu.ts
@@ -1,20 +1,36 @@
-import readlinesync = require("readline-sync");
-import { colors } from './src/util/Colors';
+import readlinesync = require("readline-sync")
 import { Conta } from "./src/util/model/Conta";
-
+import { ContaCorrente } from "./src/util/model/ContaCorrente";
+import { ContaPoupanca } from "./src/util/model/ContaPoupanca";
+import { colors } from "./src/util/Colors";
+import { ContaController } from "./src/util/controller/ContraController";
 
 export function main() {
 
-    let opcao: number;
+    //Instância da Classe ContaController
+    let contas: ContaController = new ContaController();
 
-    // Objeto da Classe Conta (Teste)
-    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
-    conta.visualizar();
-    conta.sacar(10500);
-    conta.visualizar();
-    conta.depositar(5000);
-    conta.visualizar();
+    let opcao, numero, agencia, tipo, saldo, limite, aniversario: number;
+    let titular: string;
+    const tiposContas = ['Conta Corrente', 'Conta Poupanca'];
+    
 
+    console.log("\nCriar Contas\n");
+
+    let cc1: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000, 100.0);
+    contas.cadastrar(cc1);
+
+    let cc2: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000, 100.0);
+    contas.cadastrar(cc2);
+
+    let cp1: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000, 12);
+    contas.cadastrar(cp1);
+
+    let cp2: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 126, 2, "Juliana Ramos", 8000, 15);
+    contas.cadastrar(cp2);
+
+    contas.listarTodas();
+    
     while (true) {
 
         console.log(colors.bg.black, colors.fg.yellow, 
@@ -35,8 +51,7 @@ export function main() {
         console.log("            9 - Sair                                 ");
         console.log("                                                     ");
         console.log("*****************************************************");
-        console.log("                                                     ", 
-        colors.reset);
+        console.log("                                                     ", colors.reset);
 
         console.log("Entre com a opção desejada: ");
         opcao = readlinesync.questionInt("");
@@ -51,11 +66,45 @@ export function main() {
         switch (opcao) {
             case 1:
                 console.log(colors.fg.whitestrong, "\n\nCriar Conta\n\n", colors.reset);
-                
-                keyPress()
+
+                console.log("Digite o Número da agência: ");
+                let agencia = readlinesync.questionInt("");
+
+                console.log("Digite o Nome do Titular da conta: ");
+                let titular = readlinesync.question("");
+
+                console.log("\nDigite o tipo da Conta: ");
+                const tiposContas = ["Conta Corrente", "Conta Poupança"];
+                let tipo = readlinesync.keyInSelect(tiposContas, "", { cancel: false }) + 1;
+
+                console.log("\nDigite o Saldo da conta (R$): ");
+                let saldo = readlinesync.questionFloat("");
+
+                switch (tipo) {
+                    case 1:
+                        console.log("Digite o Limite da Conta (R$): ");
+                        let limite = readlinesync.questionFloat("");
+                        contas.cadastrar(
+                            new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite)
+                        );
+                        break;
+
+                    case 2:
+                        console.log("Digite o Dia do aniversário da Conta Poupança: ");
+                        let aniversario = readlinesync.questionInt("");
+                        contas.cadastrar(
+                            new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario)
+                        );
+                        break;
+                }
+
+                keyPress();
                 break;
+
             case 2:
                 console.log(colors.fg.whitestrong, "\n\nListar todas as Contas\n\n", colors.reset);
+
+                contas.listarTodas();
 
                 keyPress()
                 break;
@@ -102,16 +151,14 @@ export function main() {
 /* Função com os dados da pessoa desenvolvedora */
 function sobre(): void {
     console.log("\n*****************************************************");
-    console.log("Projeto Desenvolvido por: ");
-    console.log("Generation Brasil - generation@generation.org");
-    console.log("github.com/conteudoGeneration");
+    console.log("Projeto Desenvolvido por: Gabrielle De Castro Alves");
+    console.log("gabrielledecastro2320@gmail.com");
+    console.log("https://github.com/gabrielleclvs");
     console.log("*****************************************************");
 }
 
 function keyPress(): void {
     console.log(colors.reset, "");
     console.log("\nPressione enter para continuar...");
-    readlinesync.prompt();
 }
-
 main();
